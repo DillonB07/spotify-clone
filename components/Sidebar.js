b810a18579d46df84e4bcb5afc378d37b3a1eadb
DillonB07 <dillonbarnes07@gmail.com>
2022-03-09 17:51:31 +0000

    Tweak sidebar colours

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,8 +4,8 @@ import {
   LibraryIcon,
   PlusCircleIcon,
   RssIcon,
-  HeartIcon,
 } from '@heroicons/react/outline'
+import { HeartIcon } from '@heroicons/react/solid'
 import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
@@ -45,15 +45,15 @@ function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         <button className="flex items-center space-x-2 hover:text-white ">
-          <PlusCircleIcon className="h-5 w-5" />
+          <PlusCircleIcon className="h-5 w-5 " />
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white ">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5 text-blue-500" />
           <p>Like Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white ">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 text-green-500" />
           <p>Search</p>
         </button>
 
