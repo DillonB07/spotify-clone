b2b24c89aba7feefeb2cace903dcba59554b7140
DillonB07 <dillonbarnes07@gmail.com>
2022-04-03 18:02:45 +0100

    Add dynamic title

--- components/Player.js
+++ components/Player.js
@@ -13,6 +13,7 @@ import {
 } from '@heroicons/react/solid'
 import { debounce } from 'lodash'
 import { useSession } from 'next-auth/react'
+import Head from 'next/head'
 import { useEffect, useCallback } from 'react'
 import { useRecoilState } from 'recoil'
 import {
@@ -114,73 +115,81 @@ function Player() {
   )
 
   return (
-    <div className="grid h-24 grid-cols-3 bg-gradient-to-b from-black to-gray-900 px-2 text-xs text-white md:px-8 md:text-base">
-      {/* left */}
-      <div className="flex items-center space-x-4">
-        <img
-          className="hidden h-10 w-10 md:inline"
-          src={songInfo?.album.images?.[0]?.url}
-          alt=""
-        />
-        <div>
-          <h3>{songInfo?.name}</h3>
-          <p>{songInfo?.artists[0]?.name}</p>
+    <>
+    <Head>
+      <title>{songInfo?.name} - {songInfo?.artists[0]?.name} | Spotify 2.0</title>
+    </Head>
+      <div className="grid h-24 grid-cols-3 bg-gradient-to-b from-black to-gray-900 px-2 text-xs text-white md:px-8 md:text-base">
+        {/* left */}
+        <div className="flex items-center space-x-4">
+          <img
+            className="hidden h-10 w-10 md:inline"
+            src={songInfo?.album.images?.[0]?.url}
+            alt=""
+          />
+          <div>
+            <h3>{songInfo?.name}</h3>
+            <p>{songInfo?.artists[0]?.name}</p>
+          </div>
+        </div>
+        {/* Center */}
+        <div className="flex items-center justify-evenly">
+          {isShuffled === false ? (
+            <SwitchHorizontalIcon onClick={handleShuffle} className="button" />
+          ) : (
+            <SwitchHorizontalIcon
+              onClick={handleShuffle}
+              className="button text-green-500"
+            />
+          )}
+          <RewindIcon
+            className="button"
+            onClick={() => spotifyApi.skipToPrevious()}
+          />
+          {isPlaying ? (
+            <PauseIcon onClick={handlePlayPause} className="button h-10 w-10" />
+          ) : (
+            <PlayIcon onClick={handlePlayPause} className="button h-10 w-10" />
+          )}
+          <FastForwardIcon
+            className="button"
+            onClick={() => spotifyApi.skipToNext()}
+          />
+          {isRepeatingState === 'off' ? (
+            <RepeatIcon onClick={handleRepeat} className="button" />
+          ) : isRepeatingState === 'context' ? (
+            <RepeatIcon
+              onClick={handleRepeat}
+              className="button text-green-500"
+            />
+          ) : (
+            <RepeatIcon
+              onClick={handleRepeat}
+              className="button text-red-500"
+            />
+          )}
+        </div>
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-3 pr-5 md:space-x-4">
+          <VolumeDownIcon
+            onClick={() => volume > 0 && setVolume(volume - 10)}
+            className="button"
+          />
+          <input
+            className="w-14 md:w-28"
+            type="range"
+            value={volume}
+            onChange={(e) => setVolume(Number(e.target.value))}
+            min={0}
+            max={100}
+          />
+          <VolumeUpIcon
+            onClick={() => volume < 100 && setVolume(volume + 10)}
+            className="button"
+          />
         </div>
       </div>
-      {/* Center */}
-      <div className="flex items-center justify-evenly">
-        {isShuffled === false ? (
-          <SwitchHorizontalIcon onClick={handleShuffle} className="button" />
-        ) : (
-          <SwitchHorizontalIcon
-            onClick={handleShuffle}
-            className="button text-green-500"
-          />
-        )}
-        <RewindIcon
-          className="button"
-          onClick={() => spotifyApi.skipToPrevious()}
-        />
-        {isPlaying ? (
-          <PauseIcon onClick={handlePlayPause} className="button h-10 w-10" />
-        ) : (
-          <PlayIcon onClick={handlePlayPause} className="button h-10 w-10" />
-        )}
-        <FastForwardIcon
-          className="button"
-          onClick={() => spotifyApi.skipToNext()}
-        />
-        {isRepeatingState === 'off' ? (
-          <RepeatIcon onClick={handleRepeat} className="button" />
-        ) : isRepeatingState === 'context' ? (
-          <RepeatIcon
-            onClick={handleRepeat}
-            className="button text-green-500"
-          />
-        ) : (
-          <RepeatIcon onClick={handleRepeat} className="button text-red-500" />
-        )}
-      </div>
-      {/* Right */}
-      <div className="flex items-center justify-end space-x-3 pr-5 md:space-x-4">
-        <VolumeDownIcon
-          onClick={() => volume > 0 && setVolume(volume - 10)}
-          className="button"
-        />
-        <input
-          className="w-14 md:w-28"
-          type="range"
-          value={volume}
-          onChange={(e) => setVolume(Number(e.target.value))}
-          min={0}
-          max={100}
-        />
-        <VolumeUpIcon
-          onClick={() => volume < 100 && setVolume(volume + 10)}
-          className="button"
-        />
-      </div>
-    </div>
+    </>
   )
 }
 
